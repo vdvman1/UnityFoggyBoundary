f9a1df92de29a98a88a7c012515228ea52b00d3a
Stefan VDV <vdvman88@gmail.com>
2018-08-20 22:27:02 +1000

    Slowdown player when past the boundary Small fixes for when fog should be rendered

--- UnityFoggyBoundary/Assets/Scripts/BoundaryFogViewer.cs
+++ UnityFoggyBoundary/Assets/Scripts/BoundaryFogViewer.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 using VDV.Spline;
 using VDV.Utility;
 
@@ -14,6 +17,8 @@ namespace VDV.FoggyBoundary
         [Range(0, 1)]
         public float MaxFog;
         public AnimationCurve FogCurve;
+        public float SlowdownEndDistance = 10;
+        public AnimationCurve SlowdownCurve;
 
         private Boundary[] boundary;
         private Transform myTransform;
@@ -22,9 +27,23 @@ namespace VDV.FoggyBoundary
         private FogMode prevFogMode;
         private float prevFogDensity;
 
+        private FirstPersonController fpsController;
+        private FieldInfo walkSpeedField;
+        private FieldInfo runSpeedField;
+        private FieldInfo moveDirField;
+
+        private float prevWalkSpeed = 0;
+        private float prevRunSpeed = 0;
+
         void Start()
         {
             myTransform = transform;
+            fpsController = GetComponent<FirstPersonController>();
+            Type type = typeof(FirstPersonController);
+            walkSpeedField = type.GetField("m_WalkSpeed", BindingFlags.NonPublic | BindingFlags.Instance);
+            runSpeedField = type.GetField("m_RunSpeed", BindingFlags.NonPublic | BindingFlags.Instance);
+            moveDirField = type.GetField("m_MoveDir", BindingFlags.NonPublic | BindingFlags.Instance);
+
             // FindGameObjectsWithTag doesn't support "Untagged"
             if (BoundaryTag == "Untagged")
             {
@@ -64,24 +83,25 @@ namespace VDV.FoggyBoundary
                     BoundaryVertex pointB = closestLine.GetPoint(min.Index + 1);
                     normal = Vector3.Lerp(normal, pointB.Normal.normalized, min.ProjectionDistance);
                 }
+                float lookDot = Vector3.Dot(myTransform.forward, normal);
                 float dot = Vector3.Dot(myTransform.position - min.Point, normal);
-                if (dot > 0)
+                if (lookDot <= 0)
                 {
-                    ShowFog(1);
+                    ResetFog();
+                }
+                else if (dot > 0)
+                {
+                    ShowFog(lookDot);
+                    if (fpsController != null)
+                    {
+                        Slowdown(min.Distance, normal, lookDot);
+                    }
                 }
                 else if (min.Distance <= FogStartDistance)
                 {
                     float fogFactor = 1 - min.Distance / FogStartDistance;
-                    dot = Vector3.Dot(myTransform.forward, normal);
-                    if (dot <= 0)
-                    {
-                        ResetFog();
-                    }
-                    else
-                    {
-                        fogFactor *= dot;
-                        ShowFog(fogFactor);
-                    }
+                    fogFactor *= lookDot;
+                    ShowFog(fogFactor);
                 }
                 else
                 {
@@ -101,6 +121,11 @@ namespace VDV.FoggyBoundary
                 RenderSettings.fog = prevFog;
                 RenderSettings.fogMode = prevFogMode;
                 RenderSettings.fogDensity = prevFogDensity;
+                if (fpsController != null)
+                {
+                    walkSpeedField.SetValue(fpsController, prevWalkSpeed);
+                    runSpeedField.SetValue(fpsController, prevRunSpeed);
+                }
             }
             inFogRange = false;
         }
@@ -112,11 +137,31 @@ namespace VDV.FoggyBoundary
                 prevFog = RenderSettings.fog;
                 prevFogMode = RenderSettings.fogMode;
                 prevFogDensity = RenderSettings.fogDensity;
+                if (fpsController != null)
+                {
+                    prevWalkSpeed = (float) walkSpeedField.GetValue(fpsController);
+                    prevRunSpeed = (float) runSpeedField.GetValue(fpsController);
+                }
             }
             inFogRange = true;
             RenderSettings.fog = true;
             RenderSettings.fogMode = FogMode.ExponentialSquared;
             RenderSettings.fogDensity = Mathf.Clamp01(Mathf.Lerp(prevFogDensity, MaxFog, FogCurve.Evaluate(fogFactor)));
+        }
+
+        private void Slowdown(float distance, Vector3 normal, float lookDot)
+        {
+            float slowdownFactor = distance / SlowdownEndDistance;
+            slowdownFactor = Mathf.Clamp01(slowdownFactor);
+            var moveDir = (Vector3) moveDirField.GetValue(fpsController);
+            moveDir.Normalize();
+            Debug.Log(moveDir);
+            float dot = Vector3.Dot(normal, moveDir);
+            slowdownFactor *= lookDot;
+            Debug.Log(lookDot);
+            slowdownFactor = SlowdownCurve.Evaluate(slowdownFactor);
+            walkSpeedField.SetValue(fpsController, Mathf.Lerp(prevWalkSpeed, 0, slowdownFactor));
+            runSpeedField.SetValue(fpsController, Mathf.Lerp(prevRunSpeed, 0, slowdownFactor));
         }
 
         void OnValidate()
